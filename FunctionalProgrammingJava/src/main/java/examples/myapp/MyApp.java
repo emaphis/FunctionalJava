9ad6f995b83a1abf47b1b449091e1778649d8529
emaphis <emaphis85@gmail.com>
2025-05-02 15:03:14 -0400

    New project based on Venkat Subramaniam's book.

---
+++ FunctionalProgrammingJava/src/main/java/examples/myapp/MyApp.java
@@ -0,0 +1,14 @@
+
+package examples.myapp;
+
+///
+/// Just an application
+///
+/// @author emaph
+///
+public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Functional Java!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FunctionalJava
+
 Practicing functional programming in Java
+
+## Exampiles from Venkat Subramamiam's "Functional Programming in Java"
+
+./FunctionalProgrammingJava
